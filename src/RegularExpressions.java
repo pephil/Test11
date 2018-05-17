29a0d4d8f82d02165146a5e45bce2d9817a1a543
pephil <philpeiser1@web.de>
2018-05-18 00:52:52 +0200

    Initial commit

---
+++ src/RegularExpressions.java
@@ -0,0 +1,19 @@
+public class RegularExpressions {
+
+    /**
+     *
+     * @param args ist die Eingabe des Benutzers
+     */
+    public static void main(String[] args) {
+
+        String command;
+        command = "1,2,3,4,5";
+        command.split(",");
+        String commandValues[] = command.split(",");
+        for (String command2 : commandValues) {
+            System.out.println(command2);
+        }
+
+    }
+
+}
